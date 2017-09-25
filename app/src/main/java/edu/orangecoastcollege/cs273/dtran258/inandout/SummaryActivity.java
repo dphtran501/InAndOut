2b655739bc60dd8c8924b6a581d29a291c88bfa1
Derek <dphtran501@yahoo.com>
2017-09-24 19:55:37 -0700

    Created SummaryActivity

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/dtran258/inandout/SummaryActivity.java
@@ -0,0 +1,15 @@
+package edu.orangecoastcollege.cs273.dtran258.inandout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SummaryActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+    }
+}
