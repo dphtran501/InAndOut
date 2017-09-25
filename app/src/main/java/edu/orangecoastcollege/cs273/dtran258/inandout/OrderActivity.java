2d6c6af440be75cf1326ea7c1869f924dd9e29d0
Derek <dphtran501@yahoo.com>
2017-09-24 17:32:52 -0700

    Initial commit

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/dtran258/inandout/OrderActivity.java
@@ -0,0 +1,15 @@
+package edu.orangecoastcollege.cs273.dtran258.inandout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrderActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
